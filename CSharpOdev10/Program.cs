462a360b0cbafe5703c432404b5953da31025d40
Halit TİRYAKİ <halittiryaki61@outlook.com>
2024-07-22 16:04:37 +0300

    Add project files.

---
+++ CSharpOdev10/Program.cs
@@ -0,0 +1,52 @@
+﻿//Geriye Değer Döndürmeyen Bir void metot.
+//Ekrana sevdiğiniz bir şarkı sözünü yazdırsın. 
+using System.Transactions;
+
+Console.WriteLine("<---------- Geriye değer döndürmeyen bir metod ---------->");
+static void song(string songText)
+{
+    Console.WriteLine(songText);
+}
+song("I found a love, for me\r\nDarling, just dive right in and follow my lead\r\nWell, I found a girl, beautiful and sweet\r\nOh, I never knew you were the someone waiting for me");
+
+
+//Geriye Tamsayı Döndüren Bir metot
+//Rastgele bir sayı üretip bu sayının 2'ye bölümünden kalanı geriye döndürsün.
+Console.WriteLine("<---------- Geriye Tamsayı Döndüren Bir metot ---------->");
+static int number(int rnd)
+{
+    int result = rnd % 2;
+    return result;
+}
+Random rnd = new Random();
+int numberRnd = rnd.Next(100);
+Console.WriteLine($"Rastgele üretilen sayı:{numberRnd}, 2ye bölümünden kalan:{number(numberRnd)}");
+
+
+//Parametre Alan ve Geriye Değer Döndüren Bir Metot
+//Parametre olarak aldığı iki sayının çarpımını geriye dönsün.
+Console.WriteLine("<---------- Parametre Alan ve Geriye Değer Döndüren Bir Metot ---------->");
+static int multiplication(int numberOne, int numberTwo)
+{
+    int result = numberOne * numberTwo;
+    return result;
+}
+Console.Write("Lütfen bir sayı giriniz: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Lütfen bir sayı giriniz: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{numberOne} x {numberTwo} = {multiplication(numberOne, numberTwo)} ");
+
+
+//Parametre Alan ve Geriye Değer Döndürmeyen bir Metot
+//Parametre olarak isim ve soyisim bilgilerini alıp " Hoş Geldiniz {isim} {soyisim} " şeklinde ekrana bassın.
+Console.WriteLine("<---------- Parametre Alan ve Geriye Değer Döndürmeyen bir Metot ---------->");
+static void welcome(string isim, string soyisim)
+{
+    Console.WriteLine($"Hoş Geldiniz {isim} {soyisim}");
+}
+Console.Write("Lütfen isminizi giriniz: ");
+string name = Console.ReadLine();
+Console.Write("Lütfen soyisminizi giriniz: ");
+string surname = Console.ReadLine();
+welcome(name, surname);
---
+++ README.md
@@ -0,0 +1,7 @@
+# C Sharp Metodlar
+
+Bu farklý metod türlerini kullanarak basit düzeyde uygulamalar yaptýk.
+Geriye Deðer Döndürmeyen Bir void metot.
+Geriye Tamsayý Döndüren Bir metot
+Parametre Alan ve Geriye Deðer Döndüren Bir Metot
+Parametre Alan ve Geriye Deðer Döndürmeyen bir Metot
